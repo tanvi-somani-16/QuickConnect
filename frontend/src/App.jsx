182934d7b716fbdbfd4d4c86979776c73107ec4c
Tanvi Somani <tanvisomani0216@gmail.com>
2025-07-17 11:46:06 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from "./components/Navbar.jsx"
+import { Routes,Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+
+      <Routes>
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
